398a66c119369beaf68e2d366b8eeac5a4979395
Eugene Hong <awidesky@naver.com>
2021-07-17 16:11:32 +0900

    show how many case testing

--- testRange.hpp
+++ testRange.hpp
@@ -5,23 +5,24 @@
 #include <thread>
 #include <vector>
 #include <utility>
+#include <iostream>
 #include <chrono>
 
 template <typename R, typename Func, typename Binop>
 R testRange(int from, int end, Func f, Binop op, int pool) {
-  std::cout << "thread number : " << std::thread::hardware_concurrency() << "\n";
-  std::cout << "\ntesting from " << std::hex << from << " to " << std::hex << end << "\n";
+  std::cout << "1thread number : " << std::thread::hardware_concurrency() << "\n";
+  std::cout << "\ntesting from " << std::hex << from << " to " << std::hex << end << std::dec << "\t(" << (end - from) << ")" << "\n";
   int cnt = (end - from) / pool;
   int to = cnt + from;
   
-  std::vector<std::future<R>> v;
-
   auto start = std::chrono::steady_clock::now();
+  std::vector<std::future<R>> v;
   for(int i = 0; i < pool; i++) {
     v.push_back(std::async(std::launch::async, [from, to, &f, &op](){
       R var = f(from);
       for(int j = from + 1; j < to; j++) {
         var = op(f(j), var);
+        if(!(j % 500)) std::cout << "\n500 iteration done!";
       }
       return var;
     }));
@@ -40,9 +41,8 @@ R testRange(int from, int end, Func f, Binop op, int pool) {
     std::cout << "\n#" << j - 1 << " task executed\n";
     re = op(v[j].get(), re);
   }
-
-  auto end = std::chrono::steady_clock::now();
-  auto diff = end - start;
+  auto end1 = std::chrono::steady_clock::now();
+  auto diff = end1 - start;
   std::cout << "\n" << std::chrono::duration <double, std::milli> (diff).count() << " ms" << std::endl;
   return re;
 }
